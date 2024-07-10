dd74c91090a5d4b75db49544788fd1ee71281bdc
David Wekesa <wekesadavid300@gmail.com>
2024-07-11 02:01:17 +0300

    Event-listeners

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('main');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    main.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListener();
